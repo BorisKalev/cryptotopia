683118f73c2fac99a0bf6ef581293b23e03d3e47
BorisKalev <64435381+BorisKalev@users.noreply.github.com>
2024-08-31 18:38:08 -0400

    Added react router dom and the Coin component in which the user will be able to open a specific coin and see it specifics

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,29 @@ import CardOptions from "./components/CardOptions";
 import Buisness from "./components/Buisness";
 import Development from "./components/Development";
 import Footer from "./components/Footer";
+import Coin from "./components/Coin";
+
+import { Routes, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Market />
-      <CardOptions />
-      <Buisness />
-      <Development />
-      <Footer />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Navbar />
+              <Hero />
+              <Market />
+              <CardOptions />
+              <Buisness />
+              <Development />
+              <Footer />
+            </>
+          }
+        />
+        <Route path="/coin" element={<Coin />} />
+      </Routes>
     </>
   );
 };
